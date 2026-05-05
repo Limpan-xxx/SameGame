d63124e8b9ebd219a606369165b3e93615ed8b08
vickeh1 <viktor.holmstrom05@gmail.com>
2026-05-05 11:38:53 +0200

    gjorde en updatepanel som är kopplad till int[][] board

--- View/GamePanel.java
+++ View/GamePanel.java
@@ -7,23 +7,37 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
     int tileSize = 35;
-    private BoardModel boardmodel; // till senare
+    JButton[][] buttons;
+    private BoardModel boardmodel;
 
     public GamePanel(BoardModel boardmodel) {
         this.boardmodel = boardmodel;
-        setPreferredSize(new Dimension(boardmodel.getGridRows() * this.tileSize, boardmodel.getGridColumns() * this.tileSize));
-        setLayout(new GridLayout(boardmodel.getGridRows(), boardmodel.getGridColumns()));
+        int rows = boardmodel.getGridRows();
+        int columns = boardmodel.getGridRows();
+        this.buttons = new JButton[rows][columns];
+
+        setPreferredSize(new Dimension(rows * this.tileSize, columns * this.tileSize));
+        setLayout(new GridLayout(rows, columns));
         setBackground(Color.DARK_GRAY);
 
         for (int i = 0; i < boardmodel.getGridRows(); i++) {
             for (int j = 0; j < boardmodel.getGridColumns(); j++) {
                 JButton Tile = new JButton();
                 Tile.setBackground(TileColor.IDtoColor(boardmodel.getBoard()[i][j].getColorID()));
+                buttons[i][j] = Tile;
                 add(Tile);
             }
         }
     }
 
+    public void updatePanel(){
+        int rows = boardmodel.getGridRows();
+        int columns = boardmodel.getGridColumns();
 
-
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                buttons[i][j].setBackground(TileColor.IDtoColor(boardmodel.getBoard()[i][j].getColorID()));
+            }
+        }
+    }
 }
