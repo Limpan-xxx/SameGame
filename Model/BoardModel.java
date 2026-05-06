becca3b31beaeb0546cc774bc26f251767ba4fd8
Limpan-xxx <linus.piaostefan@gmail.com>
2026-05-06 11:01:45 +0200

    La till rekursiv sök algoritm för att hitta tiles med samma färdID

--- Model/BoardModel.java
+++ Model/BoardModel.java
@@ -1,6 +1,5 @@
 package Model;
 
-
 import Model.Observers.GameObserver;
 
 import java.awt.*;
@@ -13,13 +12,13 @@ public class BoardModel {
 
     private ArrayList<GameObserver> observers = new ArrayList<>();
 
-    public BoardModel(int gridRows, int gridColumns){
+    public BoardModel(int gridRows, int gridColumns) {
         this.board = new Tile[gridRows][gridColumns];
         this.gridRows = gridRows;
         this.gridColumns = gridColumns;
-        for(int i = 0; i < gridRows; i++){
+        for (int i = 0; i < gridRows; i++) {
             System.out.println();
-            for(int j = 0; j < gridColumns ; j++){
+            for (int j = 0; j < gridColumns; j++) {
                 int ID = randomColorID();
                 board[i][j] = new Tile(ID);
                 System.out.print(ID + " ");
@@ -37,40 +36,57 @@ public class BoardModel {
         }
     }
 
-    public ArrayList<Point> searchConnected(int row, int column){
-        ArrayList<Point> ConnectedCoordinates = new ArrayList<>();
-
-        ConnectedCoordinates.add(new Point(row, column));
-
-        if(isTile(row-1,column) && this.board[row][column].getColorID() == this.board[row-1][column].getColorID()){ // upp
-            ConnectedCoordinates.add(new Point(row-1,column));
-        }
-        if (isTile(row+1,column) && this.board[row][column].getColorID() == this.board[row+1][column].getColorID()) { // ner
-            ConnectedCoordinates.add(new Point(row+1,column));
-        }
-        if (isTile(row,column-1) && this.board[row][column].getColorID() == this.board[row][column-1].getColorID()) { // vänster
-            ConnectedCoordinates.add(new Point(row,column-1));
-        }
-        if (isTile(row,column+1) && this.board[row][column].getColorID() == this.board[row][column+1].getColorID()) { // höger
-            ConnectedCoordinates.add(new Point(row,column+1));
-        }
-
-        return ConnectedCoordinates;
+    public ArrayList<Point> searchConnected(int row, int column) {
+        ArrayList<Point> connectedCoordinates = new ArrayList<>();
+        searchConnected(row, column, connectedCoordinates);
+        return connectedCoordinates;
     }
 
-    public boolean isTile(int row, int column){
+    private void searchConnected(int row, int column, ArrayList<Point> connectedCoordinates) {
+        Point currentPoint = new Point(row, column);
+
+        if (connectedCoordinates.contains(currentPoint)) {
+            return;
+        }
+
+        connectedCoordinates.add(currentPoint);
+
+        int colorID = this.board[row][column].getColorID();
+
+        if (isTile(row - 1, column)
+                && this.board[row - 1][column].getColorID() == colorID) {
+            searchConnected(row - 1, column, connectedCoordinates);
+        }
+
+        if (isTile(row + 1, column)
+                && this.board[row + 1][column].getColorID() == colorID) {
+            searchConnected(row + 1, column, connectedCoordinates);
+        }
+
+        if (isTile(row, column - 1)
+                && this.board[row][column - 1].getColorID() == colorID) {
+            searchConnected(row, column - 1, connectedCoordinates);
+        }
+
+        if (isTile(row, column + 1)
+                && this.board[row][column + 1].getColorID() == colorID) {
+            searchConnected(row, column + 1, connectedCoordinates);
+        }
+    }
+
+    public boolean isTile(int row, int column) {
         return row < board.length && column < board[0].length && row >= 0 && column >= 0;
     }
 
-    public void removeTile(int row, int column){
+    public void removeTile(int row, int column) {
         System.out.println();
         this.board[row][column].setColorID(0); // sets the tile to DARK_GRAY
 
         // för debug konsol
         System.out.println("removed " + row + " " + column);
-        for(int i = 0; i < gridRows; i++){
+        for (int i = 0; i < gridRows; i++) {
             System.out.println();
-            for(int j = 0; j < gridColumns ; j++){
+            for (int j = 0; j < gridColumns; j++) {
                 System.out.print(board[i][j].getColorID() + " ");
             }
         }
@@ -78,25 +94,26 @@ public class BoardModel {
         notifyObservers();
     }
 
-    public Tile[][] getBoard(){
+    public Tile[][] getBoard() {
         return board;
-    }
-    /**
-     @return random colorID
-     */
-    private int randomColorID() {
-        return (int)(Math.random() * 3) + 1; // 1=red, 2=green, 3=blue
     }
 
     /**
-     @return gridColumns
+     * @return random colorID
+     */
+    private int randomColorID() {
+        return (int) (Math.random() * 3) + 1; // 1=red, 2=green, 3=blue
+    }
+
+    /**
+     * @return gridColumns
      */
     public int getGridColumns() {
         return gridColumns;
     }
 
     /**
-     @return gridRows
+     * @return gridRows
      */
     public int getGridRows() {
         return gridRows;
