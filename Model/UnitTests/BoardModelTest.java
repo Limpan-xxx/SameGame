4a56c5847b3522f0bf96ef42ab104a9c3bc060b3
vickeh1 <viktor.holmstrom05@gmail.com>
2026-05-20 23:46:59 +0200

    added jUnit tests, linus

--- Model/UnitTests/BoardModelTest.java
+++ Model/UnitTests/BoardModelTest.java
@@ -12,19 +12,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class BoardModelTest {
 
     private BoardModel createBoard(int[][] ids) {
-        BoardModel model = new BoardModel(ids.length, ids[0].length, 3);
+    BoardModel model = new BoardModel(ids.length, ids[0].length, 3);
 
-        for (int row = 0; row < ids.length; row++) {
-            for (int col = 0; col < ids[row].length; col++) {
-                model.board[row][col] = new Tile(ids[row][col]);
+    for (int row = 0; row < ids.length; row++) {
+        for (int col = 0; col < ids[row].length; col++) {
+            model.board[row][col] = new Tile(ids[row][col]);
+        }
+    }
+
+    return model;
+}
+
+    private void assertBoardEquals(int[][] expected, BoardModel model) {
+        for (int row = 0; row < expected.length; row++) {
+            for (int col = 0; col < expected[row].length; col++) {
+                assertEquals(
+                        expected[row][col],
+                        model.board[row][col].getColorID(),
+                        "Wrong value at row " + row + ", col " + col
+                );
             }
         }
-
-        return model;
     }
 
     @Test
-    void searchConnectedShouldFindConnectedTilesWithSameColor() {
+    void searchConnected() {
         BoardModel model = createBoard(new int[][] {
                 {1, 1, 2},
                 {1, 2, 2},
@@ -37,5 +49,67 @@ class BoardModelTest {
         assertTrue(result.contains(new Point(0, 0)));
         assertTrue(result.contains(new Point(0, 1)));
         assertTrue(result.contains(new Point(1, 0)));
+    }
+
+    @Test
+    void gravityFalls() {
+        BoardModel model = createBoard(new int[][] {
+                {0, 1},
+                {2, 0},
+                {3, 4}
+        });
+
+        model.gravityFalls();
+
+        assertBoardEquals(new int[][] {
+                {0, 0},
+                {2, 1},
+                {3, 4}
+        }, model);
+    }
+
+    @Test
+    void shiftLeft() {
+        BoardModel model = createBoard(new int[][] {
+                {1, 0, 2},
+                {1, 0, 2}
+        });
+
+        model.shiftLeft();
+
+        assertBoardEquals(new int[][] {
+                {1, 2, 0},
+                {1, 2, 0}
+        }, model);
+    }
+
+    @Test
+    void hasMovesVerticalMatch() {
+        BoardModel model = createBoard(new int[][] {
+                {1, 2},
+                {1, 3}
+        });
+
+        assertTrue(model.hasMoves());
+    }
+
+    @Test
+    void hasMovesHorizontalMatch() {
+        BoardModel model = createBoard(new int[][] {
+                {1, 1},
+                {2, 3}
+        });
+
+        assertTrue(model.hasMoves());
+    }
+
+    @Test
+    void win() {
+        BoardModel model = createBoard(new int[][] {
+                {1, 2},
+                {0, 3}
+        });
+
+        assertTrue(model.win());
     }
 }
